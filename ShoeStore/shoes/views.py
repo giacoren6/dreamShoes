6a40005a7d532c1c26188305f0fea1b347828465
giacoren6 <142323106+giacoren6@users.noreply.github.com>
2024-08-15 13:04:25 +0000

    add and set up the back end python and create a python app

---
+++ ShoeStore/shoes/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Shoe
+from .serializers import ShoeSerializer
+
+
+
+class ShoeViewSet(viewsets.ModelViewSet):
+    queryset = Shoe.objects.all()
+    serializer_class = ShoeSerializer
+
+# Create your views here.
