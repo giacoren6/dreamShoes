d751734bb7e3dc994f93ea556bf39495a589451a
giacoren6 <142323106+giacoren6@users.noreply.github.com>
2024-08-18 12:27:34 +0000

    setting up

---
+++ shoe-react/src/components/ShoesList.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function ShoesList() {
+    const [shoes, setShoes] = useState([]);
+
+    useEffect(() => {
+        try {
+            axios.get('http://127.0.0.1:8000/api/shoes/')
+                .then((response) => {
+                    setShoes(response.data);
+                })
+                .catch(error => {
+                    console.error('There was an error!', error);
+                });
+        } catch (error) {
+            console.error('There was an error!', error);
+        }
+    }, []);
+
+    return (
+        <div>
+            <h1>Shoes Store</h1>
+            <ul>
+                {shoes.map((shoe) => (
+                    <li key={shoe.id}>{shoe.brand} - {shoe.model}
+                    <h2>{shoe.name} by {shoe.brand}</h2>
+                    <p>Size: {shoe.size}</p>
+                    <p>Price: £ {shoe.price}</p>
+                    <p>{shoe.description}</p>
+
+                    <imp src={shoe.image_url} alt={shoe.namem} 
+                    syle={{ with: '100px'}}/>
+
+                    
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default ShoesList;
