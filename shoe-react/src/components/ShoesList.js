99e0658d1de115b20ee5dfe21bad1a7fa1501e3e
giacoren6 <142323106+giacoren6@users.noreply.github.com>
2024-08-19 20:51:12 +0000

    adding media video

--- shoe-react/src/components/ShoesList.js
+++ shoe-react/src/components/ShoesList.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './style.css';
+
 
 function ShoesList() {
     const [shoes, setShoes] = useState([]);
@@ -19,25 +21,38 @@ function ShoesList() {
     }, []);
 
     return (
-        <div>
-            <h1>Shoes Store</h1>
-            <ul>
-                {shoes.map((shoe) => (
-                    <li key={shoe.id}>{shoe.brand} - {shoe.model}
-                    <h2>{shoe.name} by {shoe.brand}</h2>
-                    <p>Size: {shoe.size}</p>
-                    <p>Price: £ {shoe.price}</p>
-                    <p>{shoe.description}</p>
+                <div>
+                    <div className="header">
+                        <h1>Dream Shoes</h1>
+                    </div>
 
-                    <imp src={shoe.image_url} alt={shoe.namem} 
-                    syle={{ with: '100px'}}/>
+                    <div>
+                        <video autoPlay muted loop id="myVideo">
+                            <source src='components/media/shoes.mp4' type="video/mp4" />
+                        </video>
+                    </div>
 
-                    
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
-}
+                    <div className="description">
+                        <p>At Dream Shoes we have a wide range of shoes for all tastes and occasions. From trainers to high heels, we have it all. Browse our collection below.</p>
+                    </div>
+
+                    <h2 className='collection'>Our collection of shoes</h2>
+
+                    <ul>
+                        {shoes.map((shoe) => (
+                            <li key={shoe.id}>{shoe.brand} - {shoe.model}
+                                <h2>{shoe.name} by {shoe.brand}</h2>
+                                <p>Size: {shoe.size}</p>
+                                <p>Price: £ {shoe.price}</p>
+                                <p>{shoe.description}</p>
+
+                                <imp src={shoe.image_url} alt={shoe.namem}
+                                    syle={{ with: '100px' }} />
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )
+        }
 
 export default ShoesList;
